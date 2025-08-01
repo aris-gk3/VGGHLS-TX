7c48c37aa990c852f9d7da6864ee46a9c7a98e91
Aris <arisilias3@gmail.com>
2025-08-01 11:32:23 +0300

    Commit with workspace_journal.py in new hls component

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -4,3 +4,5 @@ import vitis
 client = vitis.create_client()
 client.set_workspace(path="VGGHLS-TX")
 
+comp = client.create_hls_component(name = "TL_Base",cfg_file = ["hls_config.cfg"],template = "empty_hls_component")
+
